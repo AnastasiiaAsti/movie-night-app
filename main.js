c1227673f8367bf187315116ec80cb4549824075
anastasiiaasti <astistateofmind@gmail.com>
2023-11-15 10:33:49 -0800

    adds random movie generator

--- main.js
+++ main.js
@@ -1,3 +1,23 @@
 import movieIdeas from './assets/data.js'
 
 console.log(movieIdeas);
+
+const btnEl = document.getElementById('btn');
+const movieEl = document.getElementById('movie')
+const dishEl = document.getElementById('dish')
+const cocktailEl = document.getElementById('cocktail')
+
+
+btnEl.addEventListener('click', generateIdea) 
+
+function generateIdea(){
+    const randomIndex = Math.floor(Math.random() * movieIdeas.length);
+    const randomIdea = movieIdeas[randomIndex];
+    
+    movieEl.innerText = `${randomIdea.movie}`
+    movieEl.className='neon'
+    dishEl.innerText = `${randomIdea.food}`
+    dishEl.className='neon-blue'
+    cocktailEl.innerText = `${randomIdea.cocktail}`;
+    cocktailEl.className='neon-green';
+}
