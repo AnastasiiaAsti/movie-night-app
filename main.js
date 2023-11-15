f0fad84c68feae03b3a89a1f895b7554637ef8a3
anastasiiaasti <astistateofmind@gmail.com>
2023-11-15 11:25:58 -0800

    adds colors

--- main.js
+++ main.js
@@ -6,9 +6,32 @@ const btnEl = document.getElementById('btn');
 const movieEl = document.getElementById('movie')
 const dishEl = document.getElementById('dish')
 const cocktailEl = document.getElementById('cocktail')
+const recipeEl = document.getElementById('recipe')
+const pinkEls = document.querySelectorAll('.pink')
+const blueEls = document.querySelectorAll('.blue')
+const greenEls = document.querySelectorAll('.green')
+const yellowEls = document.querySelectorAll('.yellow')
+
+
 
 
 btnEl.addEventListener('click', generateIdea) 
+
+function fillRecipe(idea) {
+    recipeEl.innerHTML = "";
+
+    idea.recipe.forEach((recipe) => {
+        let li = document.createElement("li");
+        li.innerHTML = `<h2>${recipe}</h2>`;
+        recipeEl.appendChild(li);
+    });
+}
+
+function applyClassToElements(elements, className) {
+    elements.forEach((element) => {
+        element.classList.add(className);
+    });
+}
 
 function generateIdea(){
     const randomIndex = Math.floor(Math.random() * movieIdeas.length);
@@ -20,4 +43,18 @@ function generateIdea(){
     dishEl.className='neon-blue'
     cocktailEl.innerText = `${randomIdea.cocktail}`;
     cocktailEl.className='neon-green';
+    applyClassToElements(pinkEls, 'pink-on');
+    applyClassToElements(blueEls, 'blue-on');
+    applyClassToElements(greenEls, 'green-on');
+    applyClassToElements(yellowEls, 'yellow-on');
+
+    fillRecipe(randomIdea);
+}
+
+
+function reset(){
+    movieEl.innerText = ""
+    dishEl.innerText = ""
+    cocktailEl.innerText = ""
+    recipeEl.innerHTML = ""
 }
